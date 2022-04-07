1a7be66be669a4ba7ac90a904018b08a88ad4913
BuzzMsc <99995016+BuzzMsc@users.noreply.github.com>
2022-04-07 07:38:52 +0000

    Wrote major primitive types, and for each of them I wrote 3 variable declarations. I declared 10 functions, and in the main function I wrote how these functions would look like.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-template<typename ...T>
-void ignoreUnused(T&&...) { }
+template <typename... T> void ignoreUnused(T &&...) {
+}
 
 /*
 Project 2 - Part 1 / 1
@@ -10,59 +10,88 @@ video: Chapter 2 - Part 3
 
  Create a branch named Part1
 
- Purpose:  This project will teach you how to declare variables and free functions.  
- This will be the first project where the code you write will be compiled and you will be responsible for making sure it compiles before submitting it for review.
- 
- 
+ Purpose:  This project will teach you how to declare variables and free
+functions. This will be the first project where the code you write will be
+compiled and you will be responsible for making sure it compiles before
+submitting it for review.
+ 
+ 
  1) Write down the names of the 6 major primitive types available in C++  here:
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-2) for each primitive type, write out 3 variable declarations inside the variableDeclaration() function on line 59.
-    a) give each variable declaration an initial value
-        - just ignore wchar_t. you do not need to declare 3 variables of type 'wchar_t'
-        - 'void' is a return type. you do not need to declare 3 variables of type 'void'.
-    b) at the end of the function, call ignoreUnused once and pass all of your variables to it. see line 71 for an example
- 
+ int
+ float
+ bool
+ double
+ char
+ unsigned int
+ 
+ 
+ 
+ 
+2) for each primitive type, write out 3 variable declarations inside the
+variableDeclaration() function on line 59. a) give each variable declaration an
+initial value
+		- just ignore wchar_t. you do not need to declare 3 variables of type
+'wchar_t'
+		- 'void' is a return type. you do not need to declare 3 variables of
+type 'void'. b) at the end of the function, call ignoreUnused once and pass all
+of your variables to it. see line 71 for an example
+ 
 3) Declare 10 free functions
-    each declaration should have a random number of parameters in the function parameter list.
-    When naming your parameters, choose names that are relevant to the task implied by the function's name.
+	each declaration should have a random number of parameters in the function
+parameter list. When naming your parameters, choose names that are relevant to
+the task implied by the function's name.
 
-4) add { ignoreUnused( ); } after each declaration in place of the closing semicolon
-5) pass each of your function parameters to the ignoreUnused function like you did in b)
-6) if your function returns something other than void, add 'return { };' at the end of it.
-7) provide default values for an arbitrary number of parameters in the function parameter list.
+4) add { ignoreUnused( ); } after each declaration in place of the closing
+semicolon 5) pass each of your function parameters to the ignoreUnused function
+like you did in b) 6) if your function returns something other than void, add
+'return { };' at the end of it. 7) provide default values for an arbitrary
+number of parameters in the function parameter list.
 
-8) consult the coding style guide found in the Readme.MD and adjust the formatting of your 10 functions.  At this point, you might have something that looks like this:
-    float someFunc2(bool yes, int bar=2) { ignoreUnused(yes, bar ); return { }; }
-    This does not conform with the coding standard for this course (check the Readme.MD) and needs to be corrected
-    
-9) in the main function at the end: 
-    for each of those functions declared, 
-        a) write out how the function would look if called with correct arguments
-        b) if the function returned anything, store it in a local variable via the 'auto' keyword.
-        c) pass the local variables to ignoreUnused() as you did in 2b)
-    see main() for an example of this.
- 
+8) consult the coding style guide found in the Readme.MD and adjust the
+formatting of your 10 functions.  At this point, you might have something that
+looks like this: float someFunc2(bool yes, int bar=2) { ignoreUnused(yes, bar );
+return { }; } This does not conform with the coding standard for this course
+(check the Readme.MD) and needs to be corrected
+	
+9) in the main function at the end:
+	for each of those functions declared,
+		a) write out how the function would look if called with correct
+arguments b) if the function returned anything, store it in a local variable via
+the 'auto' keyword. c) pass the local variables to ignoreUnused() as you did in
+2b) see main() for an example of this.
+ 
 10) click the [run] button.  Clear up any errors or warnings as best you can.
  */
 
-//2)
-void variableDeclarations()
-{
-    //example:
-    int number = 2; //declaration of a variable named "number", that uses the primitive type 'int', and the variable's initial value is '2'
-    
+// 2)
+void variableDeclarations() {
+	// example:
+	int number =
+		2; // declaration of a variable named "number", that uses the primitive
+		   // type 'int', and the variable's initial value is '2'
+	int myAge = 2;
+	int numberOfParticipants = 0;
+	int numberOfPlayers = 10;
+	double shoeSize = 6.50;
+	double fuelPrice = 2.12;
+	double heightRoom = 3.13;
+	bool lightIsOn = false;
+	bool coffeeIsPrepared = true;
+	bool waterIsBoiled = true;
+	float timeToLaunchRocket = 42.2f;
+	float hairThickness = 1.5f;
+	float statueHeight = 10005.32f;
+	unsigned int numberOfHousesInTown = 452;
+	unsigned int daysLeftToHolidays = 32;
+	unsigned int numberOfPetsAtHome = 3;
 
-    
-    ignoreUnused(number); //passing each variable declared to the ignoreUnused() function
+	ignoreUnused(
+		number && myAge && numberOfParticipants && numberOfPlayers &&
+		numberOfHousesInTown && daysLeftToHolidays && numberOfPetsAtHome &&
+		shoeSize && fuelPrice && heightRoom && lightIsOn && coffeeIsPrepared &&
+		waterIsBoiled && timeToLaunchRocket && hairThickness &&
+		statueHeight); // passing each variable declared to the ignoreUnused()
+					   // function
 }
 
 /*
@@ -70,93 +99,111 @@ void variableDeclarations()
  example:
  note: this example shows the result after completing steps 3-8
  */
-bool rentACar(int rentalDuration, int carType = 0)  //function declaration with random number of arguments, arbitrary number of arguments have default value
-{ 
-    ignoreUnused(rentalDuration, carType); //passing each function parameter to the ignoreUnused() function
-    return {}; //if your function returns something other than void, add 'return {};' at the end of it.
-} 
+bool rentACar(
+	int rentalDuration,
+	int carType = 0) // function declaration with random number of arguments,
+					 // arbitrary number of arguments have default value
+{
+	ignoreUnused(rentalDuration, carType); // passing each function parameter to
+										   // the ignoreUnused() function
+	return {}; // if your function returns something other than void, add
+			   // 'return {};' at the end of it.
+}
 
-/*
- 1)
- */
+bool buyAnotherPlant(int spaceForPlantsAtHome, int numberOfPlantsAtHome) {
+	ignoreUnused(spaceForPlantsAtHome, numberOfPlantsAtHome);
+	return {};
+}
 
-/*
- 2)
- */
+int daysLeftToSaturday(int numberOfTheDayInTheWeek) {
+	ignoreUnused(numberOfTheDayInTheWeek);
+	return {};
+}
 
-/*
- 3)
- */
+double howMuchTripCosted(
+	double tripDistance,
+	double fuelEffeciency,
+	double fuelPrice) {
+	ignoreUnused(tripDistance, fuelEffeciency, fuelPrice);
+	return {};
+}
 
-/*
- 4)
- */
+void playSinewave(int freqOfSinewave) {
+	ignoreUnused(freqOfSinewave);
+}
 
-/*
- 5)
- */
+void changeVolumeLevel(int volumeLevel) {
+	ignoreUnused(volumeLevel);
+}
 
-/*
- 6)
- */
+void buyFruits(int numberOfApples, int numberOfOranges) {
+	ignoreUnused(numberOfApples, numberOfOranges);
+}
 
-/*
- 7)
- */
+int numberOfHoursWorked(int startingHourOfWork, int endingHourOfWork) {
+	ignoreUnused(startingHourOfWork, endingHourOfWork);
+	return {};
+}
 
-/*
- 8)
- */
+float runningDistance(float speedOfRun, float timeOfRun) {
+	ignoreUnused(speedOfRun, timeOfRun);
+	return {};
+}
 
-/*
- 9)
- */
+void setTheOven(int ovenTemperature, int bakingTime) {
+	ignoreUnused(ovenTemperature, bakingTime);
+}
 
-/*
- 10)
- */
+bool shouldIGoOut(int temperatureOutside, bool isItRaining) {
+	ignoreUnused(temperatureOutside, isItRaining);
+	return {};
+}
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
- Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
- 
- If you didn't already: 
-    Make a pull request after you make your first commit
-    pin the pull request link and this repl.it link to our DM thread in a single message.
+ Commit your changes by clicking on the Source Control panel on the left,
+ entering a message, and click [Commit and push].
+ 
+ If you didn't already:
+	Make a pull request after you make your first commit
+	pin the pull request link and this repl.it link to our DM thread in a single
+ message.
 
  send me a DM to review your pull request when the project is ready for review.
 
  Wait for my code review.
  */
 
-int main()
-{
-    //example of calling that function, storing the value, and passing it to ignoreUnused at the end of main()
-    auto carRented = rentACar(6, 2); 
-    
-    //1)
-    
-    //2)
-    
-    //3)
-    
-    //4)
-    
-    //5)
-    
-    //6)
-    
-    //7)
-    
-    //8)
-    
-    //9)
-    
-    //10)
-    
-    
-    ignoreUnused(carRented);
-    std::cout << "good to go!" << std::endl;
-    return 0;    
+int main() {
+	// example of calling that function, storing the value, and passing it to
+	// ignoreUnused at the end of main()
+	auto carRented = rentACar(6, 2);
+
+	// 1)
+	auto anotherPlant = buyAnotherPlant(4, 2);
+	// 2)
+	auto daysToFreeDay = daysLeftToSaturday(3);
+	// 3)
+	auto tripCost = howMuchTripCosted(200.3, 4.2, 2.5);
+	// 4)
+	playSinewave(440);
+	// 5)
+	changeVolumeLevel(5);
+	// 6)
+	buyFruits(2, 50);
+	// 7)
+	auto workingHours = numberOfHoursWorked(8, 20);
+	// 8)
+	auto distanceOfRun = runningDistance(2.5f, 350.2f);
+	// 9)
+	setTheOven(180, 600);
+	// 10)
+	auto decisionAboutGoingOut = shouldIGoOut(20, false);
+
+	ignoreUnused(
+		carRented && anotherPlant && daysToFreeDay && tripCost &&
+		workingHours && distanceOfRun && decisionAboutGoingOut);
+	std::cout << "good to go!" << std::endl;
+	return 0;
 }
